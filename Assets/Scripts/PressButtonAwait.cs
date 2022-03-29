32e7738ae63a834f5c7d60432df3d4e842eed0fa
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2022-03-29 23:54:26 +0900

    first commit

---
+++ Assets/Scripts/PressButtonAwait.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+
+public class PressButtonAwait : MonoBehaviour
+{
+    [SerializeField] private Button _button;
+        [SerializeField] private Text _text;
+        private void Start()
+        {
+            Test();
+            _text.text = "ボタンは一度も押されていません！";
+        }
+    
+        private async UniTask Test()
+        {
+            for (int i=0;i<10;i++)
+            {
+                _text.text = $"{i}回ボタンが押されました！";
+                //ボタン待ち処理
+                await _button.OnClickAsync();
+            }
+    
+            _text.text = "上限回数押されました";
+        }
+
+}
